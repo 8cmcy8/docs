89bf638d44e5ef45993e9ab7a469459b577d082b
cmcy <2543927844@qq.com>
2024-06-05 21:24:51 +0800

    重现

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -33,6 +33,7 @@ export default defineConfig({
         text: '个人经历',
         items: [
           { text: '重启博客', link: '/重启博客' },
+          { text: '重现老博客文章', link: '/重现老博客文章' },
         ],
       }
     ],
